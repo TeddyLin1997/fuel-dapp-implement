5596c716e26c5d9255feeef118619a46aa1f4318
TeddyLin <i100308326@gmail.com>
2024-06-14 19:11:30 +0800

    fix: deploy error

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.resolve.alias['@'] = path.join(__dirname, './src');
     return config;
   },
 };
